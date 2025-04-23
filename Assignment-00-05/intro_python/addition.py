379a59a9c4a5eba2f72bde83b0dfe8022a64c489
Ambreenidrees6 <168377223+Ambreenidrees6@users.noreply.github.com>
2025-04-23 12:56:57 -0700

    Create addition.py

---
+++ Assignment-00-05/intro_python/addition.py
@@ -0,0 +1,13 @@
+def main():
+    print("This program adds two numbers.")
+    
+    num1 = float(input("Enter first number: "))
+    
+    num2 = float(input("Enter second number: "))
+    
+    total = num1 + num2
+
+    print(f"{num1} + {num2} = {total}")
+    print(f"The total is : {total}")
+
+main()
